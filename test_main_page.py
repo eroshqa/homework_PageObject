9be23846e2530c2a2d5908b233fd72db6725793c
eroshqa <timbalimpopo@gmail.com>
2020-01-21 16:56:24 +0300

    Add new function "is_element_present"

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,8 @@
 import time
+from .pages.main_page import MainPage
 
-#Тест поиска кнопки "Добавить в корзину"
-def test_find_button_add_basket(browser):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    time.sleep(3)
-    button_add_basket = browser.find_element_by_css_selector(".btn-add-to-basket")
-    button_text = button_add_basket.text
-    assert "" != button_text, "Button not found"
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
