09b8ac2498e95a3b94752213af3cc7585d048e17
eroshqa <timbalimpopo@gmail.com>
2020-01-22 13:02:26 +0300

    Add new locators ProductPAge

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,9 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+class ProductPageLocators():
+    BUTTON_ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
+    NAME_ADD_PRODUCT = (By.CSS_SELECTOR, ".alertinner strong")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".basket-mini")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
