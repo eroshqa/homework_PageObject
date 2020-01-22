3a758c73a8c1a59e818b8fc5f43aa5b677dce4b5
eroshqa <timbalimpopo@gmail.com>
2020-01-22 10:30:43 +0300

    Add login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         #current_url = self.current_url
-        assert "login" in self.current_url, "Not found login page"
+        assert "login" in self.browser.current_url, "Not found login page"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
