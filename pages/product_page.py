84d4b1be903eda44c4b32f0c623639049425878f
eroshqa <timbalimpopo@gmail.com>
2020-01-22 13:03:12 +0300

    Add new product_page.py

---
+++ pages/product_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_BASKET)
+        button.click()
+    def name_product_current_add_product(self):
+        current_product = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
+        add_product = self.browser.find_element(*ProductPageLocators.NAME_ADD_PRODUCT).text
+        assert current_product == add_product, "Different names products"
+    def price_basket_current_price_product(self):
+        price_basket = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        print(price_basket)
+        price_product = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        print(price_product)
+        assert price_product in price_basket, "Different price product and basket"
