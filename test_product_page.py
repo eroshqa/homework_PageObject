9210587d529f5a08f46e6c753d338192c6c821ad
eroshqa <timbalimpopo@gmail.com>
2020-01-22 13:03:50 +0300

    Add new test for product_page

---
+++ test_product_page.py
@@ -0,0 +1,13 @@
+from .pages.product_page import ProductPage
+import time
+
+def test_test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_basket()
+    page.solve_quiz_and_get_code()
+    time.sleep(3)
+    page.name_product_current_add_product()
+    page.price_basket_current_price_product()
+    time.sleep(5)
