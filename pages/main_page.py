fe43d52e29b84950df69d689ec7044fad10959c8
eroshqa <timbalimpopo@gmail.com>
2020-01-24 12:48:42 +0300

    Add new test "guest_cant_see_product_in_basket_opened_from_main_page" and "guest_cant_see_product_in_basket_opened_from_product_page"

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 
-class MainPage(BasePage)
+class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
