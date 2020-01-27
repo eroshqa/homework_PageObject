246a46db65c8624f2cbe9c056ba9a4f8a5d712f7
eroshqa <timbalimpopo@gmail.com>
2020-01-27 15:47:08 +0300

    Reformate code

--- test_product_page.py
+++ test_product_page.py
@@ -103,7 +103,7 @@ class TestUserAddToBasketFromProductPage():
         page.should_not_be_success_message()
 
     @pytest.mark.need_review
-    def test_user_guest_can_add_product_to_basket(self, browser):
+    def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
         page.open()
