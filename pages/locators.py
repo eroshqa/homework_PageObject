25bafa516e566e6da9f4fd6b4777ae2d5b1001ec
eroshqa <timbalimpopo@gmail.com>
2020-01-27 10:36:07 +0300

    Add new locators from registration form

--- pages/locators.py
+++ pages/locators.py
@@ -8,6 +8,10 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_PASSWORD_REPEAT = (By.CSS_SELECTOR, "#id_registration-password2")
+    BUTTON_REGISTRATION = (By.CSS_SELECTOR,"button[name = 'registration_submit']")
 class ProductPageLocators():
     BUTTON_ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
     NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
@@ -18,6 +22,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET = (By.CSS_SELECTOR, ".basket-mini a[href*='basket']")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 class BasketPageLocators():
     PRODUCT = (By.CSS_SELECTOR,".basket-items" )
     TEXT_EMPTY_BASKET = (By.XPATH, "//p[contains(text(), 'empty')]")
